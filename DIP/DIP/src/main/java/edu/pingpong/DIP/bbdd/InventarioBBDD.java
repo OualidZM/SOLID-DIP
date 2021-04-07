e54e459eb13a14c9430cb0dda9f76e21e21dd1c9
ozaaj <ozaaj@cifpfbmoll.eu>
2021-04-07 20:54:43 +0200

    docs: :art: Interfaces
    Finally, we have to use both of the Interfaces that we created earlier, InventarioInter.java and ControladorInven.java into ControladorEstoc.java

--- DIP/DIP/src/main/java/edu/pingpong/DIP/bbdd/InventarioBBDD.java
+++ DIP/DIP/src/main/java/edu/pingpong/DIP/bbdd/InventarioBBDD.java
@@ -1,10 +1,7 @@
 package edu.pingpong.DIP.bbdd;
 
-
 public class InventarioBBDD implements InventarioInter{
 
-
-	// Metodo de consulta a la base de datos
 	@Override
 	public int numeroProductos(String tienda, String producto) {
 		return BBDD.stocs.get(tienda).get(producto);
--- DIP/DIP/src/main/java/edu/pingpong/DIP/negocio/ControladorEstoc.java
+++ DIP/DIP/src/main/java/edu/pingpong/DIP/negocio/ControladorEstoc.java
@@ -1,20 +1,19 @@
 package edu.pingpong.DIP.negocio;
 
 
-import edu.pingpong.DIP.bbdd.InventarioBBDD;
+import edu.pingpong.DIP.bbdd.InventarioInter;
 
 public class ControladorEstoc  implements ControladorInven{
 
-	private final InventarioBBDD inventario;
+	private final InventarioInter inventario;
 	
-	public ControladorEstoc(InventarioBBDD inventario) {
+	public ControladorEstoc(InventarioInter inventario) {
 		this.inventario = inventario;
 	}
 
 	@Override
 	public boolean necesitaReponer (String tienda, String producto) {
 		int cantidadActual = inventario.numeroProductos(tienda, producto);
-		
 		return  cantidadActual < producto.length() * 100;
 	}
 
